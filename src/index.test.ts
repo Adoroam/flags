1e24ded7c050485d8cff077666cd2de5a2c3b78e
Ado <AStabile.Design@gmail.com>
2021-10-14 12:03:11 -0400

    broke it up some and updated tests for spaces

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,47 +1,111 @@
-import { flags } from './index'
+import { chDashes, rmDashes, flags } from './index'
+
+describe('chDashes', () => {
+  test('no dashes', () => {
+    expect(chDashes('apple')).toBe(0)
+  })
+  test('small flag', () => {
+    expect(chDashes('-a')).toBe(1)
+  })
+  test('small multi-flag', () => {
+    expect(chDashes('-ab')).toBe(1)
+  })
+  test('large flag', () => {
+    expect(chDashes('--apple')).toBe(2)
+  })
+  test('large flag (multiple)', () => {
+    expect(chDashes('---apple')).toBe(2)
+  })
+})
+describe('rmDashes', () => {
+  test('no dashes', () => {
+    expect(rmDashes('apple')).toBe('apple')
+  })
+  test('small flag', () => {
+    expect(rmDashes('-a')).toBe('a')
+  })
+  test('small flag caps', () => {
+    expect(rmDashes('-A')).toBe('a')
+  })
+  test('small multi-flag', () => {
+    expect(rmDashes('-ab')).toBe('ab')
+  })
+  test('small multi-flag caps', () => {
+    expect(rmDashes('-AB')).toBe('ab')
+  })
+  test('large flag', () => {
+    expect(rmDashes('--apple')).toBe('apple')
+  })
+  test('large flag caps', () => {
+    expect(rmDashes('--Apple')).toBe('apple')
+  })
+  test('large flag (multiple)', () => {
+    expect(rmDashes('---apple')).toBe('-apple')
+  })
+})
 
 describe('flags', () => {
   test('blank object when no flags supplied', () => {
     expect(flags([])).toStrictEqual({})
   })
+  test('blank object when only string', () => {
+    const mock = 'string'.split(' ')
+    expect(flags(mock)).toStrictEqual({})
+  })
+  test('blank object when only string with spaces', () => {
+    const mock = 'test string'.split(' ')
+    expect(flags(mock)).toStrictEqual({})
+  })
   test('single flag, no data', () => {
-    const mock1=['-x']
-    expect(flags(mock1)).toStrictEqual({ x: 1 })
+    const mock=['-x']
+    expect(flags(mock)).toStrictEqual({ x: 1 })
   })
   test('single flag, with data', () => {
-    const mock2 = '-x hotdog'.split(' ')
-    expect(flags(mock2)).toStrictEqual({ x: 'hotdog' })
+    const mock = '-x hotdog'.split(' ')
+    expect(flags(mock)).toStrictEqual({ x: 'hotdog' })
+  })
+  test('single flag, with spaced data', () => {
+    const mock = '-x hotdog bun'.split(' ')
+    expect(flags(mock)).toStrictEqual({ x: 'hotdog bun' })
   })
   test('two flags, no data', () => {
-    const mock3 = '-x -y'.split(' ')
-    expect(flags(mock3)).toStrictEqual({ x: 1, y: 1 })
+    const mock = '-x -y'.split(' ')
+    expect(flags(mock)).toStrictEqual({ x: 1, y: 1 })
   })
   test('two flags, with data', () => {
-    const mock4 = '-x hotdog -y bun'.split(' ')
-    expect(flags(mock4)).toStrictEqual({ x: 'hotdog', y: 'bun' })
+    const mock = '-x hotdog -y bun'.split(' ')
+    expect(flags(mock)).toStrictEqual({ x: 'hotdog', y: 'bun' })
+  })
+  test('two flags, with spaced data', () => {
+    const mock = '-x hotdog bun -y white noise'.split(' ')
+    expect(flags(mock)).toStrictEqual({ x: 'hotdog bun', y: 'white noise' })
   })
   test('two flags combined, no data', () => {
-    const mock5 = ['-xy']
-    expect(flags(mock5)).toStrictEqual({ x: 1, y: 1 })
+    const mock = ['-xy']
+    expect(flags(mock)).toStrictEqual({ x: 1, y: 1 })
   })
   test('two flags combined, with data', () => {
-    const mock6 = '-xy word'.split(' ')
-    expect(flags(mock6)).toStrictEqual({ x: 'word', y: 'word' })
+    const mock = '-xy word'.split(' ')
+    expect(flags(mock)).toStrictEqual({ x: 'word', y: 'word' })
   })
   test('large flag, no data', () => {
-    const mock7 = ['--xy']
-    expect(flags(mock7)).toStrictEqual({ xy: 1 })
+    const mock = ['--xy']
+    expect(flags(mock)).toStrictEqual({ xy: 1 })
   })
   test('large flag, with data', () => {
-    const mock8 = '--xy cat'.split(' ')
-    expect(flags(mock8)).toStrictEqual({ xy: 'cat' })
+    const mock = '--xy cat'.split(' ')
+    expect(flags(mock)).toStrictEqual({ xy: 'cat' })
   })
   test('small flag, large flag, no data', () => {
-    const mock9 = '-a --xy'.split(' ')
-    expect(flags(mock9)).toStrictEqual({ a: 1, xy: 1 })
+    const mock = '-a --xy'.split(' ')
+    expect(flags(mock)).toStrictEqual({ a: 1, xy: 1 })
   })
   test('small flag, with data large flag, with data', () => {
-    const mock10 = '-a horse --xy scooter'.split(' ')
-    expect(flags(mock10)).toStrictEqual({ a: 'horse', xy: 'scooter' })
+    const mock = '-a horse --xy scooter'.split(' ')
+    expect(flags(mock)).toStrictEqual({ a: 'horse', xy: 'scooter' })
+  })
+  test('small flag, with data large flag, with spaced data', () => {
+    const mock = '-a horse girl --xy scooter'.split(' ')
+    expect(flags(mock)).toStrictEqual({ a: 'horse girl', xy: 'scooter' })
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,34 @@
 const [, , ...args] = process.argv
-
+// finds strings starting with one or two dashes
 const dashRegex = /^\-{1,2}/i
+// function to return number of dashes for supplied string (0,1, or 2)
+export const chDashes = (str: string) => {
+  const result = dashRegex.exec(str)
+  return !!result ? result[0].length : 0
+}
+// function to remove leading dashes from supplied string
+export const rmDashes = (str: string) => str.replace(dashRegex, '').toLowerCase()
 
-export const flags = (arr = args) => {
+// uses process.argv if no array supplied
+export const flags = (arr=args) => {
+  // check to see if any flags are present
   const hasFlags = arr.some(str => dashRegex.exec(str))
+  // if no flags, return empty object
   if (!hasFlags) return {}
-  const chDashes = (str: string) => {
-    const result = dashRegex.exec(str)
-    return !!result ? result[0].length : 0
-  }
+  // a mapped copy of the array where each entry is number of dashes
   const flagMap = arr.map(chDashes)
-  const rmDashes = (str: string) => str.replace(dashRegex, '').toLowerCase()
+  // reduce function to create flags object
   return arr.reduce((a, c, i) => {
+    // check to see if entry has flags
     if (!!flagMap[i]) {
+      // get the flag or "key" by removing the dashes
       const flag = rmDashes(c)
+      // find out if flag is single dash and more than one letter
       const multiFlag = flag.length > 1 && flagMap[i] === 1
+      // this part is what needs work
+      // checks following index to see if it has a flag, if not, use as data
       const data = flagMap[i + 1] === 0 ? arr[i + 1] : 1
+      // create object for the flag/s or "key/s" and data
       const flagObject = multiFlag
         ? Object.fromEntries(flag.split('').map(l => [l, data]))
         : { [flag]: data }
