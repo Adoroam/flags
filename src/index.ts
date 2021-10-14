d880a9cdceac9f7c4de66dc925593419d413de35
Ado <AStabile.Design@gmail.com>
2021-10-14 13:55:56 -0400

    not pretty but all tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 const [, , ...args] = process.argv
 // finds strings starting with one or two dashes
 const dashRegex = /^\-{1,2}/i
+// check array to see if flags are present
+const hasFlags = (arr: string[]) => arr.some(str => dashRegex.test(str))
 // function to return number of dashes for supplied string (0,1, or 2)
 export const chDashes = (str: string) => {
   const result = dashRegex.exec(str)
@@ -9,26 +11,19 @@ export const chDashes = (str: string) => {
 // function to remove leading dashes from supplied string
 export const rmDashes = (str: string) => str.replace(dashRegex, '').toLowerCase()
 
-// uses process.argv if no array supplied
 export const flags = (arr=args) => {
-  // check to see if any flags are present
-  const hasFlags = arr.some(str => dashRegex.exec(str))
-  // if no flags, return empty object
-  if (!hasFlags) return {}
-  // a mapped copy of the array where each entry is number of dashes
-  const flagMap = arr.map(chDashes)
-  // reduce function to create flags object
-  return arr.reduce((a, c, i) => {
-    // check to see if entry has flags
+  if (!hasFlags(arr)) return {}
+  const squished = arr.map(l => !!chDashes(l) ? '@_@' + l + '@_@' : l)
+    .join(' ')
+    .split('@_@')
+    .map(x => x.trim())
+    .filter(x => x !== '')
+  const flagMap = squished.map(chDashes)
+  return squished.reduce((a, c, i) => {
     if (!!flagMap[i]) {
-      // get the flag or "key" by removing the dashes
       const flag = rmDashes(c)
-      // find out if flag is single dash and more than one letter
       const multiFlag = flag.length > 1 && flagMap[i] === 1
-      // this part is what needs work
-      // checks following index to see if it has a flag, if not, use as data
-      const data = flagMap[i + 1] === 0 ? arr[i + 1] : 1
-      // create object for the flag/s or "key/s" and data
+      const data = flagMap[i + 1] === 0 ? squished[i + 1] : 1
       const flagObject = multiFlag
         ? Object.fromEntries(flag.split('').map(l => [l, data]))
         : { [flag]: data }
